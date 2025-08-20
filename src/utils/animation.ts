e22174cf6d38d9741672bb9478bfb60022bad353
César Ádrian Hernández Hernández <cesarahh3@gmail.com>
2025-08-20 02:02:11 -0600

    feat: Refactor menu and link handling for improved interactivity and animations

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -2,37 +2,41 @@ document.addEventListener("DOMContentLoaded", () => {
     const buttonMenuIcon = document.getElementById("button-menu") as HTMLButtonElement;
     const menu = document.getElementById("menu") as HTMLUListElement;
     const heroBgMenu = document.getElementById("hero-bg-menu") as unknown as SVGElement;
+    const menuDropdown = document.getElementById("submenu-dropdown") as HTMLDivElement;
 
-    
-    
     const menuIcon = ' <svg class="md:hidden size-6 text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-align-right-icon lucide-align-right"><path d="M21 12H9"/><path d="M21 18H7"/><path d="M21 6H3"/></svg>';
     const closeIcon = '<svg class="md:hidden size-6 text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>'
 
     buttonMenuIcon.innerHTML = menuIcon;
 
-
-
     buttonMenuIcon.addEventListener("click", () => {
         if (heroBgMenu.classList.contains("hidden")) {
-            heroBgMenu.classList.remove("hidden");
-            heroBgMenu.classList.add("flex");
+            show(heroBgMenu);
             buttonMenuIcon.innerHTML = closeIcon;
         } else {
-            heroBgMenu.classList.remove("flex");
-            heroBgMenu.classList.add("hidden");
+            hide(heroBgMenu);
             buttonMenuIcon.innerHTML = menuIcon;
         }
 
         if (menu.classList.contains("hidden")) {
-            menu.classList.remove("hidden");
-            menu.classList.add("flex");
+            show(menu);
+            hide(menuDropdown);
             return;
         }
 
         if (menu.classList.contains("flex")) {
-            menu.classList.remove("flex");
-            menu.classList.add("hidden");
+            hide(menu);
             return;
         }
     });
+
+    function hide(element: HTMLUListElement | HTMLDivElement | SVGElement) {
+        element.classList.remove("flex");
+        element.classList.add("hidden");
+    }
+
+    function show(element: HTMLUListElement | HTMLDivElement | SVGElement) {
+        element.classList.remove("hidden");
+        element.classList.add("flex");
+    }
 })
--- src/utils/main.ts
+++ src/utils/main.ts
@@ -8,16 +8,13 @@ function handleLinkClick(e: Event) {
     const link = e.currentTarget as HTMLAnchorElement;
     const url = link.getAttribute("data-url");
 
-    // Si es el enlace de inicio ("/")
-    if (url === "/") {
+    if (url === "/") { // Si es el enlace de inicio ("/")
         e.preventDefault(); // Prevenir la recarga
         window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll suave al top
 
-        // Actualizar la URL sin recargar (opcional)
-        window.history.pushState({}, "", "/");
+        window.history.pushState({}, "", "/"); // Actualizar la URL sin recargar (opcional)
 
-        // Forzar la actualización de los enlaces activos
-        updateActiveLink();
+        updateActiveLink(); // Forzar la actualización de los enlaces activos
     }
 }
 
@@ -35,43 +32,44 @@ function updateActiveLink() {
 
         if (!url) return;
 
-        // Para enlaces que son hashes (empiezan con #)
+        // Para enlaces que empiezan con #
         if (url && url.startsWith("#")) {
             if (currentHash === url) {
                 if (!LINK_URLS.includes(url)) {
                     link.classList.add("text-yellow-400");
                 }
                 if (span?.classList.contains("scale-x-0")) {
-                    span?.classList.remove("scale-x-0");
-                    span?.classList.add("scale-x-100");
+                    setActiveLink(span);
                 }
             } else {
                 if (!LINK_URLS.includes(url)) {
                     link.classList.remove("text-yellow-400");
                 }
                 if (span?.classList.contains("scale-x-100")) {
-                    span?.classList.remove("scale-x-100");
-                    span?.classList.add("scale-x-0");
+                    setInactiveLink(span);
                 }
             }
-
-        }
-        // Para enlaces que son rutas (como "/")
-        else if (url === "/" && currentPath === "/" && !currentHash) {
-            // link.classList.add("text-yellow-400");
+        } else if (url === "/" && currentPath === "/" && !currentHash) { // Para enlaces que son rutas (como "/")
             if (span?.classList.contains("scale-x-0")) {
-                span?.classList.remove("scale-x-0");
-                span?.classList.add("scale-x-100");
+                setActiveLink(span);
             }
 
-        } else {
-            // link.classList.remove("text-yellow-400");
+        } else { // Para enlaces que no son hashes
             if (span?.classList.contains("scale-x-100")) {
-                span?.classList.remove("scale-x-100");
-                span?.classList.add("scale-x-0");
+                setInactiveLink(span);
             }
         }
     });
+
+    function setActiveLink(span: HTMLSpanElement) {
+        span?.classList.remove("scale-x-0");
+        span?.classList.add("scale-x-100");
+    }
+
+    function setInactiveLink(span: HTMLSpanElement) {
+        span?.classList.remove("scale-x-100");
+        span?.classList.add("scale-x-0");
+    }
 }
 
 // Detectar cambios en el hash
@@ -84,34 +82,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const menuDropdown = document.getElementById("submenu-dropdown") as HTMLDivElement;
     const buttonMenu = document.getElementById("button-submenu") as HTMLButtonElement;
+    const iconChevron = buttonMenu?.querySelector("svg") as SVGElement;
 
     buttonMenu?.addEventListener("mouseenter", (event) => {
         event.stopPropagation();
-
-        if (menuDropdown.classList.contains("hidden")) {
-            menuDropdown.classList.remove("hidden");
-            menuDropdown.classList.add("flex");
-
-            return;
-        }
-
-        if (menuDropdown.classList.contains("flex")) {
-            menuDropdown.classList.remove("flex");
-            menuDropdown.classList.add("hidden");
-
-            return;
-        }
+        if (menuDropdown.classList.contains("hidden")) return showMenuDropdown();
+        if (menuDropdown.classList.contains("flex")) return hiddenMenuDropdown();
     });
 
-    menuDropdown.addEventListener("mouseenter", () => {
-        menuDropdown.classList.remove("hidden");
-        menuDropdown.classList.add("flex");
-    });
+    menuDropdown?.addEventListener("mouseenter", showMenuDropdown);
+    menuDropdown?.addEventListener("mouseleave", hiddenMenuDropdown);
 
-    menuDropdown.addEventListener("mouseleave", () => {
+    function hiddenMenuDropdown() {
         menuDropdown.classList.remove("flex");
         menuDropdown.classList.add("hidden");
-    });
+        iconChevron.style.rotate = "0deg";
+    }
 
+    function showMenuDropdown() {
+        menuDropdown.classList.remove("hidden");
+        menuDropdown.classList.add("flex");
+        iconChevron.style.rotate = "180deg";
+    }
 });
-
