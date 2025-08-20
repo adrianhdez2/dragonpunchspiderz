bb8be88247b665de4138a7316dce18b51b0f5b16
César Ádrian Hernández Hernández <cesarahh3@gmail.com>
2025-08-20 16:40:26 -0600

    feat: Add social media links with corresponding icons to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,32 @@
+import InstagramIcon from "@/icons/InstagramIcon.astro";
+import TiktokIcon from "@/icons/TiktokIcon.astro";
+import YoutubeIcon from "@/icons/YoutubeIcon.astro";
+import FacebookIcon from "@/icons/FacebookIcon.astro";
+
+export const redes = [
+    {
+        icon: YoutubeIcon,
+        text: "@dragonpunchz",
+        url: "https://www.youtube.com/@dragonpunchz",
+    },
+    {
+        icon: TiktokIcon,
+        text: "@dragonpunchspiderz",
+        url: "https://www.tiktok.com/@dragonpunchspiderz",
+    },
+    {
+        icon: InstagramIcon,
+        text: "@seven_741",
+        url: "https://www.instagram.com/seven_741",
+    },
+    {
+        icon: FacebookIcon,
+        text: "@TheSeven741",
+        url: "https://www.facebook.com/TheSeven741",
+    },
+];
+
+
 export const LINK_NAV = [
     {
         title: "Inicio",
