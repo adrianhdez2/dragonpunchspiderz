aeda5b02be114a295ddadf07256a22055d289802
César Ádrian Hernández Hernández <cesarahh3@gmail.com>
2025-08-21 16:59:13 -0600

    feat: Limit displayed social media links to the first four and add Email and WhatsApp options

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,6 +2,8 @@ import InstagramIcon from "@/icons/InstagramIcon.astro";
 import TiktokIcon from "@/icons/TiktokIcon.astro";
 import YoutubeIcon from "@/icons/YoutubeIcon.astro";
 import FacebookIcon from "@/icons/FacebookIcon.astro";
+import EmailIcon from "@/icons/EmailIcon.astro";
+import WhatsappIcon from "@/icons/WhatsappIcon.astro";
 
 export const redes = [
     {
@@ -24,6 +26,16 @@ export const redes = [
         text: "@TheSeven741",
         url: "https://www.facebook.com/TheSeven741",
     },
+    {
+        icon: EmailIcon,
+        text: "E-mail",
+        url: "mailto:coquie_741@hotmail.com"
+    },
+    {
+        icon: WhatsappIcon,
+        text: "WhatsApp",
+        url: "https://api.whatsapp.com/send?phone=9371651880&text=Hola%20Jorge,%20me%20interesa%20tu%20trabajo"
+    }
 ];
 
 
